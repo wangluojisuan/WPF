0c2c9af7a9401cada9b2a8a1653d8054fa65edb5
WangJian <wangluojisuan@163.com>
2018-10-22 19:36:59 +0800

    Binding数据

---
+++ Binding_Student/Binding_Student/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binding_Student
+{
+    class Student : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private String name;
+
+        public String Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+                if(this.PropertyChanged!=null)
+                {
+                    this.PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Name"));
+                }
+            }
+        }
+    }
+}
