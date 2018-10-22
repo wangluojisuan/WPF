a46b88448d9484cadf91c9f0e145d724703e4a78
WangJ <wangluojisuan@163.com>
2018-10-22 22:59:53 +0800

    数据转换。

---
+++ TypeConverterTest/TypeConverterTest/Human.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConverterTest
+{
+    [TypeConverter(typeof(StringToHumanTypeConverter))]
+    class Human
+    {
+        public String Name
+        {
+            get;
+            set;
+        }
+        public Human Child
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ TypeConverterTest/TypeConverterTest/StringToHumanTypeConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConverterTest
+{
+    class StringToHumanTypeConverter : TypeConverter
+    {
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if(value is String)
+            {
+                Human h = new Human();
+                h.Name = value as String;
+                return h;
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            return true;
+        }
+    }
+}
