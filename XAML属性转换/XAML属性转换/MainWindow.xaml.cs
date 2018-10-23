48d6f9e20862b917d6185491de90b44065abd4f0
WangJ <wangluojisuan@163.com>
2018-10-23 22:56:02 +0800

    TypeConverter属性类型转换。

---
+++ XAML属性转换/XAML属性转换/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XAML属性转换
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //String str = "Name";
+            //Type strType = str.GetType();
+            //MessageBox.Show((strType == typeof(String)).ToString());
+        }
+    }
+}
