3e32348b47fbb149fe009f7e67e49fda87a1ae39
WangJ <wangluojisuan@163.com>
2018-10-24 23:45:38 +0800

    自定义标记

---
+++ WPF.Controls/WPF.Controls/MyThicknessExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace WPF.Controls
+{
+    [MarkupExtensionReturnType(typeof(Thickness))]
+    public class MyThicknessExtension : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new Thickness(this.Left,this.Top, this.Right,this.Bottom);
+        }
+
+        //public MyThicknessExtension()
+        //{
+        //    // 留空
+        //}
+
+        public double Bottom { get; set; }
+        public double Left { get; set; }
+        public double Right { get; set; }
+        public double Top { get; set; }
+    }
+}
