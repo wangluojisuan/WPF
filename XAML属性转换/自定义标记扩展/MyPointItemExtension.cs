dad760f16495da6feb078ce1f739230f5b9a31f4
WangJ <wangluojisuan@163.com>
2018-10-25 00:02:12 +0800

    改造自定义标记。

---
+++ XAML属性转换/自定义标记扩展/MyPointItemExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace 自定义标记扩展
+{
+    [MarkupExtensionReturnType(typeof(MyPointItem))]
+    class PointItemExtension : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new MyPointItem() { Longitude = this.Lon, Latitude = this.Lat };
+        }
+
+        public double Lon;
+        public double Lat;
+    }
+}
