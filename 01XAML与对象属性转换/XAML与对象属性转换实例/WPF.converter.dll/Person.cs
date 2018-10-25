563a1fa86b8121e86c04dbdb40dc103651df6696
WangJian <wangluojisuan@163.com>
2018-10-25 16:46:59 +0800

    添加“XAML与对象属性转换实例”工程 实现Extension方式的XAML与对象属性的转换。 实现TypeConverter方式的XAML与对象属性的转换。 实现WPF工程命名空间的URL变化，用http方式取代clr-namespace方式。

---
+++ 01XAML与对象属性转换/XAML与对象属性转换实例/WPF.converter.dll/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.converter.dll
+{
+    [TypeConverter(typeof(PersonConverter))]
+    public class Person
+    {
+        public String Name
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get;
+            set;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("姓名：{0}，年龄：{1}", this.Name, this.Age);
+        }
+
+        public static Person Parse(String value)
+        {
+            if(String.IsNullOrEmpty(value))
+            {
+                return new Person() { Name = "", Age = -1 };
+            }
+
+            String[] items = value.Split(',');
+            String name = items[0];
+            int age = int.Parse(items[1]);
+
+            return new Person() { Name = name, Age = age };
+        }
+    }
+}
---
+++ 01XAML与对象属性转换/XAML与对象属性转换实例/WPF.converter.dll/PersonConverter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.converter.dll
+{
+    public class PersonConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if(sourceType == typeof(String))
+            {
+                return true;
+            }
+
+            return base.CanConvertFrom(context, sourceType);
+        }
+
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+        {
+            if(destinationType == typeof(Person))
+            {
+                return true;
+            }
+
+            return base.CanConvertTo(context, destinationType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            Type sourceType = value.GetType();
+            if(this.CanConvertFrom(context, sourceType))
+            {
+                return Person.Parse(value as String);
+            }
+
+            return base.ConvertFrom(context, culture, value);
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+
+            if (this.CanConvertTo(context, destinationType))
+            {
+                Person p = value as Person;
+                if (p != null)
+                {
+                    return p.ToString();
+                }
+            }
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+    }
+}
