5d019e3f05cf4e384ae4f6059d270561acfa9b4d
WangJian <wangluojisuan@163.com>
2019-01-21 09:23:37 +0800

    Binding基础

---
+++ 02Binding/Ch06/3.7集合对象作为ItemsSource的源/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3._7集合对象作为ItemsSource的源
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ObservableCollection<Student> stuList = new ObservableCollection<Student>()
+            {
+                new Student(){Id=0, Name="Tim", Age=29},
+                new Student(){Id=1, Name="Tom", Age=28},
+                new Student(){Id=2, Name="Kyle", Age=27},
+                new Student(){Id=3, Name="Tony", Age=26},
+                new Student(){Id=4, Name="Vina", Age=25},
+                new Student(){Id=5, Name="Mike", Age=24},
+            };
+
+            this.listBoxStudents.ItemsSource = stuList;
+            //this.listBoxStudents.DisplayMemberPath = "Name";
+
+            Binding binding = new Binding("SelectedItem.Id") { Source = this.listBoxStudents };
+            this.textBoxID.SetBinding(TextBox.TextProperty, binding);
+        }
+    }
+}
---
+++ 02Binding/Ch06/3.7集合对象作为ItemsSource的源/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._7集合对象作为ItemsSource的源
+{
+    class Student
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public String Name
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get;
+            set;
+        }
+    }
+}
